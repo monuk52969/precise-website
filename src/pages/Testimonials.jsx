9cece9254c08cd2a60517314199a1426e6c91c2a
Monu Kumar <monuk52069@gmail.com>
2025-06-27 12:45:41 +0530

    Fix: Correct case for image file

--- src/pages/Testimonials.jsx
+++ src/pages/Testimonials.jsx
@@ -1,7 +1,7 @@
 import { useEffect, useRef } from 'react';
 import gsap from 'gsap';
 import { ScrollTrigger } from 'gsap/ScrollTrigger';
-import IMG from '../assets/TestIMG.jpg'
+import IMG from '../assets/Testimg.jpg'
 import { IoStar } from "react-icons/io5";
 
 gsap.registerPlugin(ScrollTrigger);
